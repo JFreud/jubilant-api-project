2168fc52b5fe287bc43ccd3780fb8d2439de3c2c
JonathanQuang <jquang@stuy.edu>
2017-11-17 13:54:54 -0500

    home basic

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-	return "what"
+	return render_template('home.html')
 
 if __name__ == '__main__':
 	app.debug = True
