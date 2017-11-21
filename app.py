77f8e6d2360f0124c1d2b187de4e161cae9a29b4
JonathanQuang <jquang@stuy.edu>
2017-11-20 23:57:25 -0500

    html template and placeholder creation stuff

--- app.py
+++ app.py
@@ -4,10 +4,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-	return render_template('home.html')
+    return redirect(url_for('home'))
+
+@app.route('/home')
+def home():
+        return render_template('home.html')
+    
+@app.route('/input')
+def input():
+        return render_template('input.html')
+
+@app.route('/output')
+def output():
+        return render_template('output.html')
+
+
+
+
 
 if __name__ == '__main__':
 	app.debug = True
 	app.run()        #runs the app
 
-print "what"
+
