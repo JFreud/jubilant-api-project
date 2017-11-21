620bd97082e6bb0ca07e10e573ad73362a566f33
JonathanQuang <jquang@stuy.edu>
2017-11-21 13:46:00 -0500

    DB insert stuff

---
+++ utils/database.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+def createTable():
+	f="data/login.db"
+	db=sqlite3.connect(f)
+	c=db.cursor()
+	command = "CREATE TABLE IF NOT EXISTS login (username TEXT, password TEXT);"   #creates users table if it doesnt exist
+	c.execute(command)
+	command = "CREATE TABLE IF NOT EXISTS userSongs(username TEXT, lastFM TEXT, tone TEXT, playlists BLOB );"  #creates posts table if it doesnt exist
+	c.execute(command)
+	db.commit()
+	db.close()       #closes and commits changes
+
+def insertIntoLoginTable(userStr,passwordStr):
+	f="data/login.db"
+	db=sqlite3.connect(f)         #connects to Datebase to allow editing
+	c=db.cursor()
+	command = "INSERT INTO login VALUES('%s','%s');"%(userStr,passwordStr)
+	c.execute(command)
+	db.commit()
+	db.close()
+
+def inserIntoUserSongs(userStr,lastFMStr,toneStr,playlistStr):
+	f="userSongs/login.db"
+	db=sqlite3.connect(f)
+	c=db.cursor()
+	command= "INSERT INTO login Values('%s','%s','%s','%s');"%(userStr,lastFMStr,toneStr,playlistStr)
+	c.execute(command)
+	db.commit()
+	db.close()
+
+
